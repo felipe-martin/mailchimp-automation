eb3f12d7d460b9c8994c49f4ccbd8527399dc790
Jaime Arroyo <jarroyo@vitamina.cl>
2023-02-08 16:24:59 -0300

    cronjobs

---
+++ ADMISSIONS_SCHEDULLING/deployment/cron_jobs/3_mark_unscheduled_process.bat
@@ -0,0 +1,8 @@
+@echo off
+echo "[INFO] /////////////// PROCESSING TO MARK NOT CHILD SCHEDULED ADAPTATIONS //////////////"
+timeout 1 > NUL
+call C:\Users\LMS\Anaconda3\Scripts\activate mailchimp
+timeout 5 > NUL
+cd C:\Users\LMS\Documents\MAILCHIMP_AUTOMATION\ADMISSIONS_SCHEDULLING\deployment\utils\
+timeout 1 > NUL
+python mark_unscheduled_process.py
