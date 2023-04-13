52fb126acdd0c658098cae2b0ccdbe5d1076c48d
jarroyo <jarroyo@vitamina.cl>
2023-04-13 09:28:54 -0400

    deployment with new app version

--- ADMISSIONS_SCHEDULLING/deployment_with_xano_v2/cron_jobs/1_insert_rows_xano_from_sql_server.bat
+++ ADMISSIONS_SCHEDULLING/deployment_with_xano_v2/cron_jobs/1_insert_rows_xano_from_sql_server.bat
@@ -3,6 +3,6 @@ echo "[INFO] /////////////// INSERTING ROWS FROM SQL SERVER VITAMINA TO XANO POS
 timeout 1 > NUL
 call C:\Users\LMS\Anaconda3\Scripts\activate mailchimp
 timeout 5 > NUL
-cd C:\Users\LMS\Documents\MAILCHIMP_AUTOMATION\ADMISSIONS_SCHEDULLING\deployment_with_xano\
+cd C:\Users\LMS\Documents\MAILCHIMP_AUTOMATION\ADMISSIONS_SCHEDULLING\deployment_with_xano_v2\
 timeout 1 > NUL
 python insert_rows_xano_from_sql_server.py
--- ADMISSIONS_SCHEDULLING/deployment_with_xano_v2/cron_jobs/3_mark_unscheduled_process.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo "[INFO] /////////////// PROCESSING TO MARK NOT CHILD SCHEDULED ADAPTATIONS //////////////"
-timeout 1 > NUL
-call C:\Users\LMS\Anaconda3\Scripts\activate mailchimp
-timeout 5 > NUL
-cd C:\Users\LMS\Documents\MAILCHIMP_AUTOMATION\ADMISSIONS_SCHEDULLING\deployment_with_xano\utils\
-timeout 1 > NUL
-python mark_unscheduled_process.py
